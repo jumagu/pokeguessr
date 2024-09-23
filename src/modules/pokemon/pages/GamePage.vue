02904aa63564b5fa8fafb4e7d416bcc12a19066b
jumagu <jmgg.gutierrez@gmail.com>
2024-09-23 15:57:11 -0500

    feat: update GamePage component to integrate all functionalities

--- src/modules/pokemon/pages/GamePage.vue
+++ src/modules/pokemon/pages/GamePage.vue
@@ -1,23 +1,37 @@
 <script setup lang="ts">
+import { GameStatus } from '../enums';
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
 
-const { rndPokemon, isLoading, gameStatus } = usePokemonGame();
+const { options, rndPokemon, isLoading, gameStatus, checkAnswer, getNextRound } = usePokemonGame();
 </script>
 
 <template>
-  <section v-if="isLoading" class="flex flex-col justify-center items-center h-screen">
+  <main v-if="isLoading || !rndPokemon" class="flex flex-col justify-center items-center h-screen">
     <h1 class="text-3xl">Please wait</h1>
     <h2 class="animate-pulse">Loading Pokemons</h2>
-  </section>
+  </main>
 
-  <section v-else class="flex flex-col justify-center items-center h-screen">
-    <h1 class="mb-5">¿What is this Pokemon?</h1>
-    <h3 class="my-2">{{ rndPokemon }}</h3>
+  <main v-else class="flex flex-col justify-center items-center min-h-screen p-6">
+    <h1 class="text-4xl text-center">What is this Pokemon?</h1>
+    <div class="h-[88px]">
+      <button
+        v-if="gameStatus !== GameStatus.Playing"
+        class="w-40 p-3 my-5 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition-colors"
+        @click="getNextRound()"
+      >
+        Play Again
+      </button>
+    </div>
 
-    <PokemonPicture />
+    <PokemonPicture :pokemon-id="rndPokemon.id" :show-pokemon="gameStatus !== GameStatus.Playing" />
 
-    <PokemonOptions />
-  </section>
+    <PokemonOptions
+      :options="options"
+      :correct-answer="rndPokemon.id"
+      :block-selection="gameStatus !== GameStatus.Playing"
+      @select-option="checkAnswer"
+    />
+  </main>
 </template>
